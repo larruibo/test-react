d2e8b7b9ee3a24cb517675670c85c97398fe1950
la.ruiz967 <la.ruiz967@uniandes.edu.co>
2020-03-29 02:25:29 -0500

    test react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Footer from "./layout/Footer.js";
+import Questions from "./components/Questions.js";
+import FormCreateQuestion from "./components/FormCreateQuestion.js";
+const initialQuestions = [
+  {
+    question: "Dummy?",
+    answers: [
+      { answer: "I'm dummy", votes: 10 },
+      { answer: "You're the dummy", votes: 2 }
+    ]
+  },
+  {
+    question: "Dummy Too?",
+    answers: [
+      { answer: "I'm dummy", votes: 10 },
+      { answer: "You're the dummy", votes: 2 }
+    ]
+  }
+];
+const App = () => {
+  const [questions, setQuestions] = useState(initialQuestions);
 
-function App() {
+  const onVote = (question, answer) => {
+    setQuestions(prevQuestions => {
+      const newQuestions = [...prevQuestions];
+
+      const qObj = newQuestions.find(q => q.question === question);
+      const newAnswers = qObj.answers.map(a =>
+        a.answer === answer ? { answer: a.answer, votes: a.votes + 1 } : a
+      );
+      qObj.answers = newAnswers;
+      return newQuestions;
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Questionator!!!</h2>
+      <div className="row">
+        <div className="col-8">
+          <Questions questions={questions} onVote={onVote} />
+        </div>
+        <div className="col-4">
+          <FormCreateQuestion />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
